d245534c935d83d679edd0c07f29c85e45051786
Ana-Gabriel <agp@odoo.com>
2020-03-23 13:07:31 -0700

    [REF] auto_warehouse: typo in manifest summary

--- auto_warehouse/__manifest__.py
+++ auto_warehouse/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "auto_warehouse",
 
-    'summary': """Auto populate the closets warehouse in SO form.""",
+    'summary': """Auto populate the closets warehouse on SO form.""",
 
     'description': """
         https://www.odoo.com/web#id=2220590&action=333&active_id=360&model=project.task&view_type=form&cids=3&menu_id=4720
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '0.3',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'stock', 'account', 'sale_management', 'account_accountant', 'mrp', 'contacts', 'sale_stock', 'sale'],
