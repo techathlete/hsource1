58aecdd448eac7e4d94ae307058e9d7691b09844
Ana-Gabriel <agp@odoo.com>
2020-03-23 12:18:47 -0700

    [ADD] auto_warehouse: add module to the repo

---
+++ auto_warehouse/models/models.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Warehouse(models.Model):
+    _inherit = 'stock.warehouse'
+
+    country_ids = fields.One2many(string='Countries Served', comodel_name='res.country',  inverse_name='warehouse_id')
+    state_ids = fields.One2many(string='States Served', comodel_name='res.country.state', inverse_name='warehouse_id')
+
+class Country(models.Model):
+    _inherit = 'res.country'
+
+    warehouse_id = fields.Many2one(string='Closest Warehouse', comodel_name='stock.warehouse', ondelete='cascade')
+
+class CountryState(models.Model):
+    _inherit = 'res.country.state'
+
+    warehouse_id = fields.Many2one(string='Closest Warehouse', comodel_name='stock.warehouse', ondelete='cascade')
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.onchange('partner_shipping_id')
+    def _change_partner_shipping_id(self):
+        Warehouse = self.env['res.country']
+        for s in self:
+            print(s.partner_shipping_id.country_id)
+            wids = Warehouse.search(['country_ids.id', '=', s['partner_shipping_id']['country_id']])
+            if wids and len(wids.ids) > 0:
+                s['warehouse_id'] = wids[0]
