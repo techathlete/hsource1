186f9921b8385afd887fbb518782563ede72835a
Ray Carnes <41073831+ray-odoo@users.noreply.github.com>
2020-03-30 15:17:54 -0700

    Initial Upload

---
+++ tis_session_timeout/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# This module and its content is copyright of Technaureus Info Solutions Pvt. Ltd.
+# - © Technaureus Info Solutions Pvt. Ltd 2020. All rights reserved.
+{
+    'name': 'Inactive Sessions Timeout',
+    'sequence': 1,
+    'version': '13.0.0.0',
+    'category': 'Tools',
+    'summary': 'Inactive Sessions Timeout',
+    'description': """
+This module disable all inactive sessions since a given delay.
+    """,
+    'author': 'Technaureus Info Solutions Pvt. Ltd.',
+    'company': 'Technaureus Info Solutions Pvt. Ltd.',
+    'website': 'https://www.technaureus.com',
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [
+        'data/ir_config_parameter_data.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'images': ['images/main_screenshot.png']
+}
